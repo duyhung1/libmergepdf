f734ab7419ad9bc26e68713163a3ebadc0de38fe
duyhung1 <33681960+duyhung1@users.noreply.github.com>
2020-07-29 12:04:49 +0700

    Update SourceInterface.php
    Get DEGREES

--- src/Source/SourceInterface.php
+++ src/Source/SourceInterface.php
@@ -22,4 +22,7 @@ interface SourceInterface
      * Get pages to fetch from source
      */
     public function getPages(): PagesInterface;
+       * Get DEGREES
+     */
+    public function getDegrees(): int;
 }
